f4afcf719b5d74ab6b47e69d05d7a06224d38d10
David <belmontecast@gmail.com>
2019-04-08 11:09:09 +0200

    nouvelle solution

---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function History () {
+    return (
+            <div>
+              <h1>Notre Histoire</h1>
+            </div>
+        );
+    }
+
+
+export default History;
